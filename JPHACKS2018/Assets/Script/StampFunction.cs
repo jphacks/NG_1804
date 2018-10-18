bc7b09673e5aadd764ca6775c5841f34028dc93d
shiro0309 <eri.a.ran.0309@outlook.jp>
2018-10-18 17:06:59 +0900

    フォルダーの作成＆コメント・スタンプの関数完成

---
+++ JPHACKS2018/Assets/Script/StampFunction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StampFunction : MonoBehaviour
+{
+
+    TouchScreenKeyboard keyboard;
+
+    void MakeARObject(string id)
+    {
+
+    }
+    //スタンプ
+    void OnTapStamp(string id)
+    {
+        string idstamp;
+        idstamp = "stamp:" + id;
+        //MakeARObject(idstamp);
+    }
+
+    //コメント
+    void OnTapComment()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        StartCoroutine(ZeroPointOneSecondWait());
+    }
+    //写真と動画
+    void OnTapPicture()
+    {
+
+    }
+
+    //コメント用のコルーチン
+    IEnumerator ZeroPointOneSecondWait()
+    {
+        while (keyboard.status != TouchScreenKeyboard.Status.Done)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        MakeARObject("comment" + keyboard.text);
+    }
+}
