29afc7cb68a24924a0438a769eb8576d23744f88
shiro0309 <eri.a.ran.0309@outlook.jp>
2018-10-18 18:49:32 +0900

    アクセス許可リクエストを合体させた形

--- JPHACKS2018/Assets/iOSScreenCapture/Scripts/UnityiOSScreenCapture.cs
+++ JPHACKS2018/Assets/iOSScreenCapture/Scripts/UnityiOSScreenCapture.cs
@@ -3,13 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Runtime.InteropServices;
 
 public class UnityiOSScreenCapture : MonoBehaviour {
 
 	public UnityEvent OnCompleteCapture;
 	public UnityEvent OnFailCapture;
 
-	public void Execute() {
+    //合体させた
+
+    [DllImport("__Internal")]
+    private static extern void _PlaySystemShutterSound();
+
+    [DllImport("__Internal")]
+    private static extern void _SendTexture(byte[] textureByte, int length);
+
+    [DllImport("__Internal")]
+    private static extern void _RequestCameraPermission();
+
+    [DllImport("__Internal")]
+    private static extern void _RequestCameraRollPermission();
+
+    [DllImport("__Internal")]
+    private static extern int _HasCameraPermission();
+
+    [DllImport("__Internal")]
+    private static extern int _HasCameraRollPermission();
+
+    [DllImport("__Internal")]
+    private static extern void _GoToSettings();
+
+    public static void PlaySystemShutterSound()
+    {
+        _PlaySystemShutterSound();
+    }
+
+    public static void SaveTexture(byte[] textureByte, int length)
+    {
+        _SendTexture(textureByte, length);
+    }
+    public static void RequestPermissions()
+    {
+        AVAuthorizationStatus avstatus = HasCameraPermission();
+        PHAuthorizationStatus phstatus = HasCameraRollPermission();
+
+        //アクセス許可のリクエストを出していない場合はリクエストを送る
+        if (avstatus == AVAuthorizationStatus.NotDetermined)
+        {
+            _RequestCameraPermission();
+        }
+
+        if (phstatus == PHAuthorizationStatus.NotDetermined)
+        {
+            _RequestCameraRollPermission();
+        }
+    }
+
+
+    public void Execute() {
+        //合体させた
+        RequestPermissions();
+
+
+
 #if !UNITY_EDITOR
 		PHAuthorizationStatus phstatus = (PHAuthorizationStatus)Enum.ToObject(
 			typeof(PHAuthorizationStatus), UnityiOS.HasCameraRollPermission());
@@ -22,7 +78,7 @@ public class UnityiOSScreenCapture : MonoBehaviour {
 			OnFailCapture.Invoke();
 		}
 #endif
-	}
+    }
 
 	private IEnumerator _CaptureScreenShot() {
 		//canvasGroup.alpha = 0; //みたいな処理を入れておくと撮影時にUIを外すといった事が出来ます
@@ -50,4 +106,40 @@ public class UnityiOSScreenCapture : MonoBehaviour {
 			OnFailCapture.Invoke();
 		}
 	}
+
+
+    /*合体させた部分*/
+    public static AVAuthorizationStatus HasCameraPermission()
+    {
+#if !UNITY_EDITOR
+		return (AVAuthorizationStatus)Enum.ToObject(
+   				typeof(AVAuthorizationStatus), _HasCameraPermission());
+#endif
+        return AVAuthorizationStatus.Authorized;
+    }
+
+    public static PHAuthorizationStatus HasCameraRollPermission()
+    {
+#if !UNITY_EDITOR
+        return (PHAuthorizationStatus)Enum.ToObject(
+				typeof(PHAuthorizationStatus), _HasCameraRollPermission());
+#endif
+        return PHAuthorizationStatus.Authorized;
+    }
+
+    public static void GoToSettings()
+    {
+#if !UNITY_EDITOR
+        _GoToSettings();
+#endif
+    }
+
+    public void GoToSettings_forUGUI()
+    {
+#if !UNITY_EDITOR
+        _GoToSettings();
+#endif
+    }
+
+    /*合体させた部分*/
 }
