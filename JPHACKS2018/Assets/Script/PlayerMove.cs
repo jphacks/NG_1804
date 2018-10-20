bbb3b34ba1f4169b8ddd986bf0bc7d7fb5fc21cc
air <mturo5536@docomo.ne.jp>
2018-10-20 22:45:36 +0900

    title後の移動追加

---
+++ JPHACKS2018/Assets/Script/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerMove : MonoBehaviour {
+    public GameObject player;
+
+    private int timer = 0;
+
+    private void Start()
+    {
+        StartCoroutine(DelayMethod(0.1f));
+    }
+    // Update is called once per frame
+    void Update () {
+        Transform mytransform = player.transform;
+        Vector3 Pos = mytransform.position;
+        if (Pos.x <= 70 && timer == 1)
+        {       transform.position += new Vector3(0.4f, 0.4f, 0);
+                transform.Rotate(new Vector3(-0.5f, 0, 0));
+        }
+	}
+
+    IEnumerator DelayMethod(float time)
+    {
+        yield return new WaitForSeconds(time);
+        timer += 1;
+    }
+}
