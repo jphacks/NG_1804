6fe6d47ffdac6b6c7f4ff02e68790ce5f879666a
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2018-10-20 11:11:22 +0900

    ボタン機能の追加、Imageの追加

--- JPHACKS2018/Assets/Script/buttonreference.cs
+++ JPHACKS2018/Assets/Script/buttonreference.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class buttonreference : MonoBehaviour {
-    public GameObject MainButton;
+    public GameObject StampButton;
+    public GameObject ImageButton;
+    public GameObject CommentButton;
     public bool mode;
 	// Use this for initialization
 	void Start () {
@@ -12,15 +14,22 @@ public class buttonreference : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-            mode = MainButton.activeSelf;
+            mode = StampButton.activeSelf;
         }
         public void OnClick()
         {
         if (mode == true)
         {
-            MainButton.SetActive(false);
+            StampButton.SetActive(false);
+            ImageButton.SetActive(false);
+            CommentButton.SetActive(false);
         }
-         
+         if (mode == false)
+        {
+            StampButton.SetActive(true);
+            ImageButton.SetActive(true);
+            CommentButton.SetActive(true);
+        }
         }
     
 }
