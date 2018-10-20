b100510e5a2304e501c091a2de0e4676af3ef791
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2018-10-20 10:53:01 +0900

    メイン画面にボタン追加、ボタン用スクリプト(書きかけを追加)

---
+++ JPHACKS2018/Assets/Script/buttonreference.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonreference : MonoBehaviour {
+    public GameObject MainButton;
+    public bool mode;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            mode = MainButton.activeSelf;
+        }
+        public void OnClick()
+        {
+        if (mode == true)
+        {
+            MainButton.SetActive(false);
+        }
+         
+        }
+    
+}
